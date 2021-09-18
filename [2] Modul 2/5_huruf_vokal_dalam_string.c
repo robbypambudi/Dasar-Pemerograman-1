03256685a3189ff3d269a16e1e02bf0bfb7ffa3f
RobbyPambudi <robby.pambudi10@gmail.com>
2021-09-18 12:19:25 +0700

    Huruf Vokal Dalam String

--- [2] Modul 2/5_huruf_vokal_dalam_string.c
+++ [2] Modul 2/5_huruf_vokal_dalam_string.c
@@ -8,37 +8,37 @@ kemudian dihitung berapa banyak bilangan vokal yang terdapat pada string tersebu
 
 int main (){
     char kata[1000];
-    gets(kata);
+    gets(kata);                                                  // mendatkan banyak kata
     int banyak_huruf, panjang_kata = strlen(kata);
     
-    int vokal[5]    = {'a', 'i', 'u', 'e', 'o'},
-        vokal_2[5]  = {'A', 'I', 'U', 'E', 'O'};
+    int vokal[5]    = {'a', 'i', 'u', 'e', 'o'},                 // Huruf Vokal biasa
+        vokal_2[5]  = {'A', 'I', 'U', 'E', 'O'};                 // Huruf Vokal Kapital
 
-    for (int i = 0; i < 5; i++){
-        int banyak_kata = 0;
+    for (int i = 0; i < 5; i++){        
+        int banyak_vokal = 0;                                    // Variabel Banya
 
-        for (int j = 0; j < panjang_kata; j++){
-            if((vokal[i]==kata[j]) || (vokal_2[i] == kata[j])){
-                banyak_kata+=1;
+        for (int j = 0; j < panjang_kata; j++){     
+            if((vokal[i]==kata[j]) || (vokal_2[i] == kata[j])){     // Mengambil data di array
+                banyak_vokal+=1;                                    // Jika memenuhi nilanya akan bertambah
             }
         }
 
-        switch (i)
+        switch (i)                                                  // Selesai
         {
         case 0:
-            printf("A/a : %d\n", banyak_kata);
+            printf("A/a : %d\n", banyak_vokal);
             break;
         case 1:
-            printf("I/i : %d\n", banyak_kata);
+            printf("I/i : %d\n", banyak_vokal);
             break;
         case 2:
-            printf("U/u : %d\n", banyak_kata);
+            printf("U/u : %d\n", banyak_vokal);
             break;
         case 3:
-            printf("E/e : %d\n", banyak_kata);
+            printf("E/e : %d\n", banyak_vokal);
             break;
         case 4:
-            printf("O/o : %d\n", banyak_kata);
+            printf("O/o : %d\n", banyak_vokal);
             break;
         
         default:
