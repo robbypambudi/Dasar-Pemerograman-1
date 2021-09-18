8143ea30a2e6ca8982a66776d3f8f98f85fc2520
RobbyPambudi <robby.pambudi10@gmail.com>
2021-09-18 12:04:46 +0700

    Cetak Bintang Jika Prima

--- [2] Modul 2/3_cetak_bintang_part_2.c
+++ [2] Modul 2/3_cetak_bintang_part_2.c
@@ -1,36 +1,38 @@
 /* 
 Membuat program apabila bertemu dengan bilangan prima akan mencetak *
 dan untuk bilangan non prima akan mencetak angka
+
+antara 2 - N
 */
 
 #include <stdio.h>
 
 int main (){
-    int x;                      // Inisialisasi Variabel
+    int x;                                               // Inisialisasi Variabel
 
     
     scanf("%d", &x);
-    for (int i = 2; i <= x; i++){           // Perhitungan dimulai
+    for (int i = 2; i <= x; i++){                       // Perhitungan dimulai
 
-        if (i == 2 || i == 3 || i == 5 || i == 7){
+        if (i == 2 || i == 3 || i == 5 || i == 7){      // Bilangan prima dasar
             printf("* ");
         }
-        else if (i%2==0){
+        else if (i%2==0){                               // Deteksi Bilangan genap
             printf("%d ", i);
         }
         else{
-            int memori_banyak_pembagian = 1;      // memori untuk bantak pembagian
-            for (int j = 2; j <= i; j++)
+            int dapat_dibagi_sebanyak = 1;            // memori untuk bantak pembagian
+            for (int j = 2; j <= i; j++)                // Jika Bilangan Ganjil dan Terindikasi Bil Prima 
             {
-                if (i%j==0){
-                    memori_banyak_pembagian++;
+                if (i%j==0){                            
+                    dapat_dibagi_sebanyak++;          // Jika hasil mod = 0 maka memorinya akan bertambah 1
                 }
             }
-            if (memori_banyak_pembagian < 3){
+            if (dapat_dibagi_sebanyak < 3){         // Jika hanya dapat dibagi oleh 2 bilangan maka prima
                 printf("* ");
             }
             else{
-                printf("%d ", i);
+                printf("%d ", i);                   // Jika Tidak Maka Bilangan ganjil
             }
             
         }
