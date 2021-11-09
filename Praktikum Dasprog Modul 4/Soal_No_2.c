9a2285a95dcd55d5c7945c9d923a50733d58ae31
Robby Pambudi <robby.pambudi10@gmail.com>
2021-11-09 15:47:22 +0700

    Soal No 2

--- Praktikum Dasprog Modul 4/Soal_No_2.c
+++ Praktikum Dasprog Modul 4/Soal_No_2.c
@@ -30,18 +30,22 @@ void ReadAll(struct Data *data, int n)
 
 void Delete(struct Data *data, int angka, int n)
 {
-    if ((n >= angka) && (data[angka - 1].status == true))
+    if (angka < 0)
     {
-        data[angka - 1].status = false;
+        printf("Data tidak ditemukan\n");
+    }
+    else if ((n >= angka) && (data[angka].status == true))
+    {
+        data[angka].status = false;
         printf("Data telah dihapus\n");
     }
-    else if ((n >= angka) && (data[angka - 1].status == false))
+    else if ((n >= angka) && (data[angka].status == false))
     {
         printf("Data sudah kosong\n");
     }
     else if (n < angka)
     {
-        printf("Data tidak ditemukann\n");
+        printf("Data tidak ditemukan\n");
     }
     printf("\n");
 }
@@ -68,7 +72,15 @@ void Read(struct Data *data, int X, int Y, int N)
 {
     for (int i = X - 1; i < Y; i++)
     {
-        if ((i < N) && (data[i].status == true))
+        if (i < 0)
+        {
+            printf("Data tidak ditemukan\n");
+        }
+        else if (i >= N)
+        {
+            printf("Data tidak ditemukan\n");
+        }
+        else if ((i < N) && (data[i].status == true))
         {
             printf("%s %05d %d %d %d\n", data[i].nama, data[i].id, data[i].jumlah_mobil, data[i].jumlah_motor, data[i].jumlah_speda);
         }
@@ -76,45 +88,32 @@ void Read(struct Data *data, int X, int Y, int N)
         {
             printf("Data telah dihapus\n");
         }
-        else if (i >= N)
-        {
-            printf("Data tidak ditemukan\n");
-        }
     }
     printf("\n");
-
-    return;
 }
 
 void Create(struct Data *data, int *N, char nama[], int id, int data1, int data2, int data3)
 {
     // Cek apakah sudah ada
-    for (int i = 0; i < *N; i++)
-    {
-        if (data[i].id == id)
-        {
-            printf("Data telah ditambahkan\n");
-            break;
-        }
-        if ((data[i].id != id) && (i == *N - 1))
-        {
-            strcpy(data[*N].nama, nama);
-            data[*N].id = id;
-            data[*N].jumlah_mobil = data1;
-            data[*N].jumlah_motor = data2;
-            data[*N].jumlah_speda = data3;
-            data[*N].status = true;
-            *N += 1;
-            printf("Data telah ditambahkan\n");
-            break;
-        }
-    }
+
+    strcpy(data[*N].nama, nama);
+    data[*N].id = id;
+    data[*N].jumlah_mobil = data1;
+    data[*N].jumlah_motor = data2;
+    data[*N].jumlah_speda = data3;
+    data[*N].status = true;
+    *N += 1;
+    printf("Data telah ditambahkan\n");
 }
 int main()
 {
     int n, t;
     scanf("%d %d", &n, &t);
     struct Data data[60];
+    if (n < 1 && t < 1)
+    {
+        return 0;
+    }
 
     for (int i = 0; i < n; i++)
     {
@@ -137,7 +136,7 @@ int main()
         {
             scanf("%s", nama_perintah);
 
-            if (strcmp(nama_perintah, "All") >= 0)
+            if (strcmp(nama_perintah, "All") == 0)
             {
                 ReadAll(data, n);
             }
@@ -159,7 +158,7 @@ int main()
         else if (strcmp(Perintah, "Delete") == 0)
         {
             scanf("%d", &angka);
-            Delete(data, angka, n);
+            Delete(data, angka - 1, n);
         }
         // Untuk Update
         else if (strcmp(Perintah, "Update") == 0)
@@ -169,15 +168,22 @@ int main()
             for (int i = 0; i < n; i++)
             {
                 alamat = i;
-                if (id == data[i].id)
+                if ((id == data[i].id) && (data[i].status == true))
                 {
                     Perbarui(data, nama, data1, data2, data3, alamat);
                     break;
                 }
-                if (i == n - 1 && data[i].id != id)
+                else if ((id == data[i].id) && (data[i].status == false))
                 {
-                    printf("Data tidak Ditemukan\n");
+                    printf("Data tidak ditemukan\n");
                     printf("\n");
+                    break;
+                }
+                if ((i == n - 1) && (data[i].id != id))
+                {
+                    printf("Data tidak ditemukan\n");
+                    printf("\n");
+                    break;
                 }
             }
         }
